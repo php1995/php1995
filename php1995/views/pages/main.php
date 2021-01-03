ef066299d1b1870caef5b7cc28583d344da5bc22
Victor <victoraretinskiy@gmail.com>
2021-01-03 14:40:06 +0300

    update css

--- php1995/views/pages/main.php
+++ php1995/views/pages/main.php
@@ -6,21 +6,21 @@
         <span>Victor Kaspersky &reg;</span>
     </div>
     <div id="about">
-        <a href="" onclick="slowScroll('#contact')">Contacts</a>
-        <a href="" onclick="slowScroll('#main')">Projects</a>
-        <a href="" onclick="slowScroll('#main')">About me</a>
+        <a href="#contacts" onclick="slowScroll('#contacts')">Contacts</a>
+        <a href="#professions" onclick="slowScroll('#main')">Professions</a>
+        <a href="#overview" onclick="slowScroll('#main')">About me</a>
     </div>
 </header>
 
 <div id="top">
-    <h1>Programing</h1>
+    <h1><?php echo htmlspecialchars("<h1>")?><span style="color: olivedrab">Programing</span><?php echo htmlspecialchars("</h1>")?></h1>
     <h3>is like way to live</h3>
 </div>
 
 <div id="main">
     <div class="intro">
         <h2>Let`s develop, lazy ass!</h2>
-        <span>or go watch stupid videos... https://youtube.com </span>
+        <span>or still decreasing... https://tictok.com </span>
     </div>
     <div class="text">
         <span>
@@ -32,30 +32,6 @@
 </div>
 <?php require_once "public/blocks/overview.html" ?>
 <?php require_once "public/blocks/contacts.html" ?>
-<div id="professions">
-    <div>
-        <span lang="heading">Who are you?</span>
-        <div class="profession_class"><span>WEB</span>
-            <input type="radio" class="profession_item">Fullstack developer</input>
-            <input type="radio" class="profession_item">Frontend developer</input>
-            <input type="radio" class="profession_item">Backend developer</input>
-        </div>
-
-        <div class="profession_class"><span>Mobile</span>
-            <input type="radio" class="profession_item">IOS developer</input>
-            <input type="radio" class="profession_item">Android developer</input>
-        </div>
-
-        <div class="profession_class"><span>Enterprise</span>
-            <input type="radio" class="profession_item">Java developer</input>
-            <input type="radio" class="profession_item">C# developer</input>
-        </div>
-
-        <div class="profession_class"><span>Game dev</span>
-            <input type="radio" class="profession_item">C++ developer</input>
-            <input type="radio" class="profession_item">C# developer</input>
-        </div>
-    </div>
-</div>
+<?php require_once "public/blocks/professions.html" ?>
 </body>
 </html>
